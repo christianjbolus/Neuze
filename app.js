5e713158a77d110e2e6f38fdc65a0319700ad966
Christian Bolus <christianjbolus@gmail.com>
2021-05-17 10:57:31 -0400

    Add logic for API calls

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-keyword = 'bitcoin'
+let keyword = 'bitcoin';
+let imgPath = null;
+const apiKey = 'fjc5OVaxAFce0CdOsFdAoV1Tu46z6XWC'
 
-const BASE_URL = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=fjc5OVaxAFce0CdOsFdAoV1Tu46z6XWC';
-const SEARCH_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${keyword}&api-key=fjc5OVaxAFce0CdOsFdAoV1Tu46z6XWC`
-const IMG_URL = `static01.nyt.com/${url}`
+const url = {
+    base: 'https://api.nytimes.com/svc/topstories/v2/home.json',
+    search: `https://api.nytimes.com/svc/search/v2/articlesearch.json`,
+    img: `static01.nyt.com/${imgPath}`,
+};
 
-async function getData() {
+
+async function getTopStories() {
     try {
-        const res = await axios.get(SEARCH_URL);
-        console.log(res);
+        let reqUrl = buildUrl(url.base)
+        const res = await axios.get(reqUrl);
+        let data = res.data.results
+        console.log(data);
     } catch (err) {
         console.log(err);
     }
 }
 
-getData();
+async function getSearchResults() {
+    try {
+        let reqUrl = buildUrl(url.search, keyword)
+        const res = await axios.get(reqUrl);
+        let data = res.data.response.docs
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// getTopStories();
+// getSearchResults()
+
+function buildUrl (url, searchParam) {
+    const newUrl = new URL(url)
+    newUrl.searchParams.append('api-key', apiKey)
+    if (searchParam) newUrl.searchParams.append('q', searchParam)
+    return newUrl
+}
