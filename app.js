9256b43a4ff9b732e564ae871372dd8d566942b6
Christian Bolus <christianjbolus@gmail.com>
2021-05-17 11:49:33 -0400

    Get user input and append to API call

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const searchForm = document.querySelector('#search-form');
+const input = document.querySelector('input');
+
 let keyword = 'bitcoin';
 let imgPath = null;
-const apiKey = 'fjc5OVaxAFce0CdOsFdAoV1Tu46z6XWC'
+const apiKey = 'fjc5OVaxAFce0CdOsFdAoV1Tu46z6XWC';
 
 const url = {
     base: 'https://api.nytimes.com/svc/topstories/v2/home.json',
@@ -8,23 +11,28 @@ const url = {
     img: `static01.nyt.com/${imgPath}`,
 };
 
+searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    let keyword = input.value;
+    // getSearchResults(keyword);
+});
 
 async function getTopStories() {
     try {
-        let reqUrl = buildUrl(url.base)
+        let reqUrl = buildUrl(url.base);
         const res = await axios.get(reqUrl);
-        let data = res.data.results
+        let data = res.data.results;
         console.log(data);
     } catch (err) {
         console.log(err);
     }
 }
 
-async function getSearchResults() {
+async function getSearchResults(keyword) {
     try {
-        let reqUrl = buildUrl(url.search, keyword)
+        let reqUrl = buildUrl(url.search, keyword);
         const res = await axios.get(reqUrl);
-        let data = res.data.response.docs
+        let data = res.data.response.docs;
         console.log(data);
     } catch (err) {
         console.log(err);
@@ -34,9 +42,9 @@ async function getSearchResults() {
 // getTopStories();
 // getSearchResults()
 
-function buildUrl (url, searchParam) {
-    const newUrl = new URL(url)
-    newUrl.searchParams.append('api-key', apiKey)
-    if (searchParam) newUrl.searchParams.append('q', searchParam)
-    return newUrl
+function buildUrl(url, searchParam) {
+    const newUrl = new URL(url);
+    newUrl.searchParams.append('api-key', apiKey);
+    if (searchParam) newUrl.searchParams.append('q', searchParam);
+    return newUrl;
 }
