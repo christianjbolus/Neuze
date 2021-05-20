04bb94069440a06171fa23fc909bac2ddc07f0f3
Christian Bolus <christianjbolus@gmail.com>
2021-05-20 11:20:11 -0400

    Add logic to remove article from local storage

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const searchForm = document.querySelector('#search-form');
 const input = document.querySelector('input');
-const validation = document.querySelector('.validation')
+const validation = document.querySelector('.validation');
 const articleContainer = document.querySelector('.article-container');
-const myArticles = document.querySelector('#my-articles')
+const myArticles = document.querySelector('#my-articles');
 
 const API_KEY = 'fjc5OVaxAFce0CdOsFdAoV1Tu46z6XWC';
-let READ_LIST = 0
+let READ_LIST = 0;
 
 const URLS = {
     base: 'https://api.nytimes.com/svc/topstories/v2/home.json',
@@ -13,53 +13,48 @@ const URLS = {
     img: 'http://static01.nyt.com',
 };
 
-
 searchForm.addEventListener('submit', e => {
     e.preventDefault();
     if (input.value === '') {
-        validation.style.display = 'block'
+        validation.style.display = 'block';
     } else {
-        validation.style.display = 'none'
+        validation.style.display = 'none';
         let keyword = input.value.trim();
         clearArticles(articleContainer);
         renderSearchComponent(keyword);
-        input.value = ''
+        input.value = '';
     }
 });
 
-
 myArticles.addEventListener('click', () => {
     let savedArticles = getSavedArticles();
-    clearArticles(articleContainer)
-    renderSavedArticles(savedArticles)
-})
-
+    clearArticles(articleContainer);
+    renderSavedArticles(savedArticles);
+});
 
 function listenForRenderModal() {
-    const cards = document.querySelectorAll('.card')
+    const cards = document.querySelectorAll('.card');
     for (let card of cards) {
-        card.addEventListener('click', function() {
-            renderModal(this)
-            listenForCloseModal()
-            listenForBookmark()
-        })
+        card.addEventListener('click', function () {
+            renderModal(this);
+            listenForCloseModal();
+            listenForBookmark();
+        });
     }
 }
 
-
 function listenForCloseModal() {
-    const modal = document.querySelector('.modal-container')
-    const close = document.querySelector('#close')
-    window.addEventListener('click', (e) => {
+    const modal = document.querySelector('.modal-container');
+    const close = document.querySelector('#close');
+    window.addEventListener('click', e => {
         if (e.target == modal || e.target == close) {
-            modal.classList.remove('active')
+            modal.classList.remove('active');
             setTimeout(() => {
-                articleContainer.firstElementChild.remove()
-            }, 300)
+                articleContainer.firstElementChild.remove();
+            }, 300);
         }
-    })
+    });
 }
-
 
 async function getTopStories() {
     try {
@@ -72,19 +67,17 @@ async function getTopStories() {
     }
 }
 
-
 async function getSearchResults(keyword) {
     try {
         let reqUrl = buildUrl(URLS.search, keyword);
         const res = await axios.get(reqUrl);
         let data = res.data.response.docs;
-        if (!data[0]) renderError()
+        if (!data[0]) renderError();
         return data;
     } catch (err) {
         console.log(err);
     }
 }
-
 
 async function renderMainComponent() {
     let response = await getTopStories();
@@ -105,36 +98,14 @@ async function renderMainComponent() {
             articleContainer.insertAdjacentHTML('beforeend', component);
         }
     }
-    listenForRenderModal()
-}
-
-
-function renderSavedArticles(response) {
-    for (let article of response) {
-        if (article.multimedia[0]) {
-            let component = `
-                <div class="card">
-                    <div class="card-img" style="background-image: url('${article.multimedia[0].url}')"></div>
-                    <div class="card-body">
-                        <h3 class="headline">${article.title}</h3>
-                        <p class="byline hidden">${article.byline}</p>
-                        <p class="publish-date">${formatDate(article.published_date)}</p>
-                        <p class="lead-paragraph hidden">${article.abstract}</p>
-                        <p class="article-link hidden">${article.url}</p>
-                    </div>
-                </div>
-                `;
-            articleContainer.insertAdjacentHTML('beforeend', component);
-        }
-    }
-    listenForRenderModal()
+    listenForRenderModal();
 }
 
 
 async function renderSearchComponent(keyword) {
     let response = await getSearchResults(keyword);
     for (let article of response) {
-        if (article.multimedia[0]) {    //! WORK ON THIS CONDITIONAL
+        if (article.multimedia[0]) {      //! WORK ON THIS CONDITIONAL
             let component = `
                 <div class="card">
                     <div class="card-img" style="background-image: url('${URLS.img}/${article.multimedia[0].url}')"></div>
@@ -150,36 +121,59 @@ async function renderSearchComponent(keyword) {
             articleContainer.insertAdjacentHTML('beforeend', component);
         }
     }
-    listenForRenderModal()
+    listenForRenderModal();
 }
 
+function renderSavedArticles(response) {
+    for (let article of response) {
+        if (article.multimedia[0]) {
+            let component = `
+                <div class="card">
+                    <div class="card-img" style="background-image: url('${article.multimedia[0].url}')"></div>
+                    <div class="card-body">
+                        <h3 class="headline">${article.title}</h3>
+                        <p class="byline hidden">${article.byline}</p>
+                        <p class="publish-date">${formatDate(article.published_date)}</p>
+                        <p class="lead-paragraph hidden">${article.abstract}</p>
+                        <p class="article-link hidden">${article.url}</p>
+                        <p class="hidden">${article.id}</p>
+                        <p class="hidden">${article.saved}</p>
+                    </div>
+                </div>
+                `;
+            articleContainer.insertAdjacentHTML('beforeend', component);
+        }
+    }
+    listenForRenderModal();
+}
 
 function renderModal(element) {
     let modal = `
         <div class="modal-container">
-            <div class="modal">
-                <div class="modal-img" style="background-image: ${element.firstElementChild.style.backgroundImage.replaceAll('"', '\'')}"></div>
+            <div class="modal" id="${element.lastElementChild.children[5]?.textContent}">
+                <div class="modal-img" style="background-image: ${element.firstElementChild.style.backgroundImage.replaceAll('"', "'")}"></div>
                 <div class="modal-body">
                     <h3 class="modal-headline">${element.lastElementChild.firstElementChild.textContent}</h3>
                     <h4 class="byline">${element.lastElementChild.children[1].textContent}</h4>
                     <p class="modal-publish-date">${element.lastElementChild.children[2].textContent}</p>
                     <p class="lead-paragraph">${element.lastElementChild.children[3].textContent}</p>
-                    <a href="${element.lastElementChild.lastElementChild.textContent}" target="_blank"><button id="modal-btn" class="btn">Full Article</button></a>
+                    <a href="${element.lastElementChild.children[4].textContent}" target="_blank"><button id="modal-btn" class="btn">Full Article</button></a>
                     <div class="modal-control">
-                        <i id="bookmark" class="far fa-bookmark"></i>
+                        <i id="bookmark" class="${element.lastElementChild.children[6]?.textContent === 'true' ? 'fas' : 'far'} fa-bookmark"></i>
                         <i id="close" class="fas fa-times"></i>
                     </div>  
                 </div
             </div>
         </div> 
-        `
-    articleContainer.insertAdjacentHTML('afterbegin', modal)
-    let modalContainer = document.querySelector('.modal-container')
+        `;
+    articleContainer.insertAdjacentHTML('afterbegin', modal);
+    let modalContainer = document.querySelector('.modal-container');
     setTimeout(() => {
-        modalContainer.classList.add('active')
-    }, 100)
+        modalContainer.classList.add('active');
+    }, 100);
 }
 
+// 
 
 function clearArticles(element) {
     while (element.lastChild) {
@@ -187,12 +181,10 @@ function clearArticles(element) {
     }
 }
 
-
 function formatDate(date) {
     let formattedDate = new Date(date).toDateString().slice(4);
     return formattedDate;
 }
-
 
 function buildUrl(url, searchParam) {
     const newUrl = new URL(url);
@@ -201,62 +193,74 @@ function buildUrl(url, searchParam) {
     return newUrl;
 }
 
-
 function renderError() {
-    let error = '<div class="error"></div>'
-    articleContainer.insertAdjacentHTML('afterbegin', error)
+    let error = '<div class="error"></div>';
+    articleContainer.insertAdjacentHTML('afterbegin', error);
 }
 
-
-renderMainComponent()
-
+// renderMainComponent()
 
 //========================================================================
 //* PSEUDOCODE FOR READLIST
 
 // Create event listener on modal bookmark icon
 function listenForBookmark() {
-   let bookmark = document.querySelector('#bookmark')
-   bookmark.addEventListener('click', function () {
-       let modal = this.closest('.modal')
-       let article = createArticleObject(modal)
-       saveArticle(article)
-   })
+    let bookmark = document.querySelector('#bookmark');
+    bookmark.addEventListener('click', function () {
+        toggleClass(this, 'far', 'fas');
+        let modal = this.closest('.modal');
+        if (this.classList.contains('fas')) {
+            let article = createArticleObject(modal);
+            saveArticle(article);
+        } else {
+            localStorage.removeItem(modal.id)
+        }
+    });
 }
 
 // Create object and store aticle data
-function createArticleObject (element) {
+function createArticleObject(element) {
     let articleObj = {
+        id: READ_LIST,
         multimedia: [{ url: trimUrl(element.firstElementChild.style.backgroundImage) }],
         title: element.lastElementChild.firstElementChild.textContent,
         byline: element.lastElementChild.children[1].textContent,
         published_date: element.lastElementChild.children[2].textContent,
         abstract: element.lastElementChild.children[3].textContent,
-        url: element.lastElementChild.children[4].href
-    }
-    return (articleObj)
+        url: element.lastElementChild.children[4].href,
+        saved: true
+    };
+    return articleObj;
 }
 
 // Covert object to JSON string and save in local storage
-function saveArticle (article) {
-    localStorage.setItem(READ_LIST.toString(), JSON.stringify(article))
-    READ_LIST++
+function saveArticle(article) {
+    localStorage.setItem(READ_LIST.toString(), JSON.stringify(article));
+    READ_LIST++;
 }
 
 // Create read list button in nav
 // ✅
 
 // Create function to pull from all articles from local storage
-function getSavedArticles () {
-    let savedArticles = []
+function getSavedArticles() {
+    let savedArticles = [];
     for (let i = 0; i < READ_LIST; i++) {
-        savedArticles.push(JSON.parse(localStorage.getItem(i.toString())))
+        savedArticles.push(JSON.parse(localStorage.getItem(i.toString())));
     }
-    return savedArticles
+    return savedArticles;
 }
 // Parse each object (JSON.parse) and push into readlist array
+// ✅
 
 // Render each article object.
+// ✅
+
+// Create function to remove article from local storage
+function removeSavedArticle(id) {
+    
+}
+// Create function to clear local storage
 
 //=========================================================================
 //* LOCAL STORAGE PRACTICE
@@ -267,15 +271,23 @@ function getSavedArticles () {
 //         firstPara: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptate consectetur doloremque, atque dignissimos in architecto corrupti aspernatur repellat officiis reprehenderit dolor non et inventore ipsum explicabo dolorum voluptates a. Consequuntur, eum itaque sapiente iure maxime placeat nam corporis quas odio. Hic fugiat quo sunt quod, consequatur dignissimos nihil. Ipsam."
 //     }
 
-
 // localStorage.setItem('0', JSON.stringify(article))
 // console.log(JSON.parse(localStorage.getItem('0')))
-
 
 // let bookmark = document.querySelector('#bookmark')
 
 // let modal = bookmark.closest('.modal')
 
-function trimUrl (url) {
-    return url.slice(5, url.length - 2)
+function trimUrl(url) {
+    return url.slice(5, url.length - 2);
+}
+
+function toggleClass(element, class1, class2) {
+    if (element.classList.contains(class1)) {
+        element.classList.remove(class1);
+        element.classList.add(class2);
+    } else {
+        element.classList.remove(class2);
+        element.classList.add(class1);
+    }
 }
